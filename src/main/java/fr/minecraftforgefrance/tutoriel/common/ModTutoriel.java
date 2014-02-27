b58b02efd324ab1f5fb0b0ddae88519c4b7772a6
kevin68 <baumeyerkevin@gmx.fr>
2014-02-27 14:46:19 +0100

    Item

--- src/main/java/fr/minecraftforgefrance/tutoriel/common/ModTutoriel.java
+++ src/main/java/fr/minecraftforgefrance/tutoriel/common/ModTutoriel.java
@@ -1,5 +1,7 @@
 package fr.minecraftforgefrance.tutoriel.common;
 
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -7,31 +9,35 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.common.registry.GameRegistry;
 import fr.minecraftforgefrance.tutoriel.proxy.CommonProxy;
 
-@Mod(modid = "modtutoriel", name = "Mod Tutoriel", version = "1.0.0")
-
+@Mod(modid = ModTutoriel.MODID, name = "Mod Tutoriel", version = "1.0.0")
 public class ModTutoriel
 {
-	@Instance("modtutoriel")
+	public static final String MODID = "modtutoriel";
+
+	@Instance(MODID)
 	public static ModTutoriel instance;
-	
+
 	@SidedProxy(clientSide = "fr.minecraftforgefrance.tutoriel.proxy.ClientProxy", serverSide = "fr.minecraftforgefrance.tutoriel.proxy.CommonProxy")
 	public static CommonProxy proxy;
-	
+
+	public static Item itemTutoriel;
+
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
 	{
-
+		itemTutoriel = new ItemTutoriel().setUnlocalizedName("itemTutoriel").setTextureName(ModTutoriel.MODID + ":itemtutoriel").setCreativeTab(CreativeTabs.tabMaterials);
+		GameRegistry.registerItem(itemTutoriel, "itemtutoriel", ModTutoriel.MODID);
 	}
-	
+
 	@EventHandler
 	public void init(FMLInitializationEvent event)
 	{
 		proxy.registerRender();
 	}
-	
+
 	@EventHandler
 	public void postInit(FMLPostInitializationEvent event)
 	{
