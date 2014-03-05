4bfbd3e50f346591c556c63905e48ea0a39c66fa
robin4002 <robin4002@gmail.com>
2014-03-05 02:14:49 +0100

    Ajout de deux bloc basique

---
+++ src/main/java/fr/minecraftforgefrance/tutoriel/common/BlockTutoriel.java
@@ -0,0 +1,12 @@
+package fr.minecraftforgefrance.tutoriel.common;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class BlockTutoriel extends Block
+{
+	protected BlockTutoriel(Material material)
+	{
+		super(material);
+	}
+}
--- src/main/java/fr/minecraftforgefrance/tutoriel/common/ModTutoriel.java
+++ src/main/java/fr/minecraftforgefrance/tutoriel/common/ModTutoriel.java
@@ -1,7 +1,11 @@
 package fr.minecraftforgefrance.tutoriel.common;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
@@ -10,6 +14,7 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
 import fr.minecraftforgefrance.tutoriel.proxy.CommonProxy;
 
 @Mod(modid = ModTutoriel.MODID, name = "Mod Tutoriel", version = "1.0.0")
@@ -24,6 +29,7 @@ public class ModTutoriel
 	public static CommonProxy proxy;
 
 	public static Item itemTutoriel, itemTutoriel2;
+	public static Block blockTutoriel, blockTutoriel2;
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event)
@@ -33,6 +39,12 @@ public class ModTutoriel
 
 		GameRegistry.registerItem(itemTutoriel, "item_tutoriel");
 		GameRegistry.registerItem(itemTutoriel2, "item_tutoriel2");
+
+		blockTutoriel = new BlockTutoriel(Material.rock).setBlockName("tutoriel").setBlockTextureName(MODID + ":block_tutoriel").setCreativeTab(CreativeTabs.tabBlock);
+		blockTutoriel2 = new BlockTutoriel(Material.wood).setBlockName("tutoriel2").setBlockTextureName(MODID + ":block_tutoriel2").setCreativeTab(CreativeTabs.tabBlock);
+
+		GameRegistry.registerBlock(blockTutoriel, "block_tutoriel");
+		GameRegistry.registerBlock(blockTutoriel2, "block_tutoriel2");
 	}
 
 	@EventHandler
