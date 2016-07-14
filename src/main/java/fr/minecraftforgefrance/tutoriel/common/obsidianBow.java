a400d7a5f0bcc5126b13606a5110e71719a7b7ee
robin4002 <robin4002@gmail.com>
2016-07-15 00:24:32 +0200

    Ajout d'un arc

---
+++ src/main/java/fr/minecraftforgefrance/tutoriel/common/obsidianBow.java
@@ -0,0 +1,188 @@
+package ma.Yeyvo.MVC.common;
+
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
+import net.minecraft.init.Items;
+import net.minecraft.item.EnumAction;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBow;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.player.ArrowLooseEvent;
+import net.minecraftforge.event.entity.player.ArrowNockEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class ObsidianBow extends ItemBow
+{
+	public static final String[] bowPullIconNameArray = new String[] {"1", "2", "3"};
+    @SideOnly(Side.CLIENT)
+    private IIcon[] iconArray;
+    
+
+    public ObsidianBow()
+    {
+        this.maxStackSize = 1;
+        this.setMaxDamage(700);
+        this.setFull3D();
+    }
+
+    /**
+     * called when the player releases the use item button. Args: itemstack, world, entityplayer, itemInUseCount
+     */
+    
+    public void onPlayerStoppedUsing(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer, int par4)
+    {
+        int j = this.getMaxItemUseDuration(par1ItemStack) - par4;
+
+        ArrowLooseEvent event = new ArrowLooseEvent(par3EntityPlayer, par1ItemStack, j);
+        MinecraftForge.EVENT_BUS.post(event);
+        if (event.isCanceled())
+        {
+            return;
+        }
+        j = event.charge;
+
+        boolean flag = par3EntityPlayer.capabilities.isCreativeMode || EnchantmentHelper.getEnchantmentLevel(Enchantment.infinity.effectId, par1ItemStack) > 0;
+
+        if (flag || par3EntityPlayer.inventory.hasItem(Items.arrow))
+        {
+            float f = (float)j / 20.0F;
+            f = (f * f + f * 2.0F) / 3.0F;
+
+            if ((double)f < 0.1D)
+            {
+                return;
+            }
+
+            if (f > 1.0F)
+            {
+                f = 1.0F;
+            }
+
+            EntityArrow entityarrow = new EntityArrow(par2World, par3EntityPlayer, f * 3.0F);
+
+            if (f == 1.0F)
+            {
+                entityarrow.setIsCritical(true);
+            }
+
+            int k = EnchantmentHelper.getEnchantmentLevel(Enchantment.power.effectId, par1ItemStack);
+
+            if (k > 0)
+            {
+                entityarrow.setDamage(entityarrow.getDamage() + (double)k * 1D + 1D);
+            }
+
+            int l = EnchantmentHelper.getEnchantmentLevel(Enchantment.punch.effectId, par1ItemStack);
+
+            if (l > 0)
+            {
+                entityarrow.setKnockbackStrength(0);
+            }
+
+            if (EnchantmentHelper.getEnchantmentLevel(Enchantment.flame.effectId, par1ItemStack) > 0)
+            {
+                entityarrow.setFire(130);
+            }
+
+            par1ItemStack.damageItem(1, par3EntityPlayer);
+            par2World.playSoundAtEntity(par3EntityPlayer, "random.bow", 1.0F, 1.0F / (itemRand.nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+
+            if (flag)
+            {
+                entityarrow.canBePickedUp = 2;
+            }
+            else
+            {
+                par3EntityPlayer.inventory.consumeInventoryItem(Items.arrow);
+            }
+
+            if (!par2World.isRemote)
+            {
+                par2World.spawnEntityInWorld(entityarrow);
+            }
+        }
+    }
+    /**
+     * How long it takes to use or consume an item
+     */
+    
+    public int getMaxItemUseDuration(ItemStack par1ItemStack)
+    {
+        return 8000; //temps avant chaque utilisation
+    }
+
+    /**
+     * returns the action that specifies what animation to play when the items is being used
+     */
+    
+    public EnumAction getItemUseAction(ItemStack par1ItemStack)
+    {
+        return EnumAction.bow;
+    }
+    /**
+     * Called whenever this item is equipped and the right mouse button is pressed. Args: itemStack, world, entityPlayer
+     */
+    public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer)
+    {
+        ArrowNockEvent event = new ArrowNockEvent(par3EntityPlayer, par1ItemStack);
+        MinecraftForge.EVENT_BUS.post(event);
+        if (event.isCanceled())
+        {
+            return event.result;
+        }
+
+        if (par3EntityPlayer.capabilities.isCreativeMode || par3EntityPlayer.inventory.hasItem(Items.arrow))
+        {
+            par3EntityPlayer.setItemInUse(par1ItemStack, this.getMaxItemUseDuration(par1ItemStack));
+        }
+
+        return par1ItemStack;
+    }
+
+    /**
+     * Return the enchantability factor of the item, most of the time is based on material.
+     */
+    public int getItemEnchantability()
+    {
+        return 1;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister par1IconRegister)
+    {
+        this.itemIcon = par1IconRegister.registerIcon(this.getIconString());
+        this.iconArray = new IIcon[bowPullIconNameArray.length];
+
+        for (int i = 0; i < this.iconArray.length; ++i)
+        {
+            this.iconArray[i] = par1IconRegister.registerIcon(this.getIconString() + "_" + bowPullIconNameArray[i]);
+        }
+    }
+
+    /**
+     * used to cycle through icons based on their used duration, i.e. for the bow
+     */
+    @Override
+    public IIcon getIcon(ItemStack stack, int renderPass, EntityPlayer player, ItemStack usingItem, int useRemaining)
+    {
+    if(usingItem != null && usingItem.getItem().equals(this))
+    {
+    int k = usingItem.getMaxItemUseDuration() - useRemaining;
+    if(k >= 18)
+    return iconArray[2];
+    if(k > 13)
+    return iconArray[1];
+    if(k > 0)
+    return iconArray[0];
+    }
+    return getIconIndex(stack);
+    }
+}
